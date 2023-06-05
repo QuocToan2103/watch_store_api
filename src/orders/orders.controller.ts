d98334d4682f5a6fb87aaaa8d870ec78cabe939e
QuocToan2103 <104415521+QuocToan2103@users.noreply.github.com>
2023-06-05 10:27:21 +0700

    Update orders.controller.ts

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,41 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+   constructor(private readonly ordersService: OrdersService) {}
+    @Get()
+    async getOrders(@Query('q') q: string, @Query('page') page=1){
+       const orders = await this.ordersService.getAll(page,q)
+       return orders;
+    }
+
+    @Get(':id')
+    async getOrderById(@Param('id') id: number){
+       const order = await this.ordersService.getById(id)
+       return order
+    }
+
+    async updateOrder(@Param('id') id: number,@Body() orderData: UpdateOrderDto,@UploadedFile() image?: Express.Multer.File){
+
+        const order = await this.ordersService.update(id,orderData)
+        return order;
+    }
+
+    @Delete(':id')
+    async deleteOrder(@Param('id') id: number){
+        await this.ordersService.delete(id)
+        return "Order has been deleted"
+    }
+   
+
+    @Post()
+    async createOrder(@Body() orderData: any) {
+      // Lưu đơn hàng vào cơ sở dữ liệu
+      const order = await this.ordersService.create(orderData);
+  
+      // Gửi email thông báo đến khách hàng
+      await this.ordersService.sendOrderNotification(orderData.email, order);
+  
+      return order;
+    }
+}
